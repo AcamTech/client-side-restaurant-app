730f4647b63606ae8ec0cbf1aadf3b63113c1ea1
Andrés Roberto <andresroberto10@gmail.com>
2016-04-11 10:55:57 -0500

    Improve orders report view

--- src/components/restaurant-orders-list.js
+++ src/components/restaurant-orders-list.js
@@ -8,48 +8,38 @@ export default function RestaurantOrdersList(){
         <thead>
           <tr>
             <th>Fecha</th>
+            <th>Creada</th>
+            <th>Procesada</th>
+            <th>Entregada</th>
             <th>Mesa</th>
             <th>Mesero</th>
             <th>Valor</th>
             <th>Comentarios</th>
-            <th>Calificación del servicio</th>
-            <th colSpan="3"></th>
+            <th>Calificación</th>
           </tr>
         </thead>
         <tbody>
           <tr>
-            <td>Abril 24, 1992</td>
-            <th>1</th>
-            <th>Tony Banderas</th>
-            <th>$19.000</th>
-            <th>Ninguno</th>
-            <th>5.0/5.0</th>
-            <td>
-              <a className="no-decoration color-success" href="#"><span className="icon-pencil"></span> Editar</a>
-            </td>
-            <td>
-              <a className="no-decoration color-danger" href="#"><span className="icon-x"></span> Eliminar</a>
-            </td>
-            <td>
-              <a className="no-decoration color-info" href="#"> Ver Platos</a>
-            </td>
-          </tr>
-          <tr>
-            <td>Octubre 18, 1951</td>
+            <td>ABR 24 2016</td>
+            <td>19:31</td>
+            <td>19:33</td>
+            <td>20:20</td>
             <th>2</th>
             <th>Mike Jackson</th>
             <th>$45.000</th>
             <th>La comida estaba bien, pero demoró mucho en estar lista.  </th>
-            <th>4.7/5.0</th>
-            <td>
-              <a className="no-decoration color-success" href="#"><span className="icon-pencil"></span> Editar</a>
-            </td>
-            <td>
-              <a className="no-decoration color-danger" href="#"><span className="icon-x"></span> Eliminar</a>
-            </td>
-            <td>
-              <a className="no-decoration color-info" href="#"> Ver Platos</a>
-            </td>
+            <th>4.7</th>
+          </tr>
+          <tr>
+            <td>ABR 24 2016</td>
+            <td>22:22</td>
+            <td>22:25</td>
+            <td>22:30</td>
+            <th>1</th>
+            <th>Tony Banderas</th>
+            <th>$19.000</th>
+            <th>Ninguno</th>
+            <th>5.0</th>
           </tr>
         </tbody>
       </table>
