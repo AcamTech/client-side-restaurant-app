a367fbdb2d2e91528ba8a16c3447568edf373378
Andrés Roberto <andresroberto10@gmail.com>
2016-05-06 07:00:12 -0500

    Fix links for admin pages

--- src/components/restaurant-layout/main-menu.js
+++ src/components/restaurant-layout/main-menu.js
@@ -5,22 +5,22 @@ export default function MainMenu({restaurantId}){
   return (
     <ul className="main-navigation nav">
       <li className="main-navigation__item nav__item">
-        <Link className="main-navigation__link nav__link" activeClassName="is-active" to={`/restaurante/${restaurantId}/personal`}>
+        <Link className="main-navigation__link nav__link" activeClassName="is-active" to={`/restaurante/${restaurantId}/admin/personal`}>
           <span className="icon-note icon-lg" title="Restaurantes"></span> <span className="main-navigation__text">Personal</span>
         </Link>
       </li>
       <li className="main-navigation__item nav__item">
-        <Link className="main-navigation__link nav__link" activeClassName="is-active" to={`/restaurante/${restaurantId}/mesas`}>
+        <Link className="main-navigation__link nav__link" activeClassName="is-active" to={`/restaurante/${restaurantId}/admin/mesas`}>
           <span className="icon-note icon-lg" title="Restaurantes"></span> <span className="main-navigation__text">Mesas</span>
         </Link>
       </li>
       <li className="main-navigation__item nav__item">
-        <Link className="main-navigation__link nav__link" activeClassName="is-active" to={`/restaurante/${restaurantId}/menu`}>
+        <Link className="main-navigation__link nav__link" activeClassName="is-active" to={`/restaurante/${restaurantId}/admin/menu`}>
           <span className="icon-note icon-lg" title="Restaurantes"></span> <span className="main-navigation__text">Menu</span>
         </Link>
       </li>
       <li className="main-navigation__item nav__item">
-        <Link className="main-navigation__link nav__link" activeClassName="is-active" to={`/restaurante/${restaurantId}/reportes`}>
+        <Link className="main-navigation__link nav__link" activeClassName="is-active" to={`/restaurante/${restaurantId}/admin/reportes`}>
           <span className="icon-note icon-lg" title="Restaurantes"></span> <span className="main-navigation__text">Reporte</span>
         </Link>
       </li>
