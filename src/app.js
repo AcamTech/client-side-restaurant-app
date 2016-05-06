c84f154c8cc0c7e1f7d7c3b48d08c8b90ff65ee4
Richard Roncancio <batusai513@msn.com>
2016-05-05 22:38:36 -0500

    move the store back to index.js

--- src/app.js
+++ src/app.js
@@ -1,85 +1,17 @@
-import React from 'react';
+import React, {PropTypes} from 'react';
 import { Provider } from 'react-redux';
-import { Router, browserHistory } from 'react-router';
 import getRoutes from './routes';
-import {checkAuthed, getMember} from 'helpers/auth';
-import configureStore from './store/configureStore';
-import {redirectToUserRoot} from 'actions/staff';
+import { checkIfAuthed }  from '/';
 
-const {store, history} = configureStore({}, browserHistory);
-
-var routesPrivileges = {
-  admin: /^admin/,
-  waiter: /^ordenes/,
-  kitchen: /^cocina/
-};
-
-function isValidRoute(member, path) {
-  var matches = path.match(/\/restaurante\/([^\/]*)\/(.*)/);
-
-  var restaurantId = matches[1];
-  var shortPath = matches[2];
-
-  if (restaurantId === member.restaurant) {
-    return routesPrivileges[member.role].test(shortPath);
-  } else {
-    return false;
-  }
-}
-
-function checkIfAuthed(nextState, replace, callback){
-  var isAuthed = checkAuthed(store);
-  var nextStatePath = nextState.location.pathname;
-
-  var staff = store.getState().staff;
-  var member = staff.list[staff.authedId];
-
-  // TODO: Clean this thing
-  if (nextStatePath == '/login' || nextStatePath == '/') {
-    if (isAuthed == true) {
-      if (member && member.role) {
-        redirectToUserRoot(member, replace);
-        callback();
-      } else {
-        getMember(staff.authedId)
-          .then((member) => {
-            redirectToUserRoot(member, replace);
-            callback();
-          });
-      }
-    } else {
-      callback();
-    }
-  } else {
-    if (isAuthed == false) {
-      replace('/login');
-    } else {
-      if (member && member.role) {
-        if (isValidRoute(member, nextStatePath)) {
-          callback();
-        } else {
-          redirectToUserRoot(member, replace);
-          callback();
-        }
-      } else {
-        getMember(staff.authedId)
-          .then((member) => {
-            if (isValidRoute(member, nextStatePath)) {
-              callback();
-            } else {
-              redirectToUserRoot(member, replace);
-              callback();
-            }
-          });
-      }
-    }
-  }
-}
-
-export default function App(){
+export default function App({store, history}){
   return (
     <Provider store={store}>
       {getRoutes(history, checkIfAuthed)}
     </Provider>
   );
 }
+
+App.propTypes = {
+  store: PropTypes.object.isRequired,
+  history: PropTypes.object.isRequired
+};
--- src/index.js
+++ src/index.js
@@ -1,14 +1,88 @@
 import { AppContainer } from 'react-hot-loader';
 import React from 'react';
 import {render} from 'react-dom';
+import { browserHistory } from 'react-router';
+import configureStore from './store/configureStore';
 import App from './app';
 import './styles/main.scss';
+import {redirectToUserRoot} from 'actions/staff';
+import {checkAuthed, getMember} from 'helpers/auth';
+
+const {store, history} = configureStore({}, browserHistory);
+
+var routesPrivileges = {
+  admin: /^admin/,
+  waiter: /^ordenes/,
+  kitchen: /^cocina/
+};
+
+function isValidRoute(member, path) {
+  var matches = path.match(/\/restaurante\/([^\/]*)\/(.*)/);
+
+  var restaurantId = matches[1];
+  var shortPath = matches[2];
+
+  if (restaurantId === member.restaurant) {
+    return routesPrivileges[member.role].test(shortPath);
+  } else {
+    return false;
+  }
+}
+
+export function checkIfAuthed(nextState, replace, callback){
+  var isAuthed = checkAuthed(store);
+  var nextStatePath = nextState.location.pathname;
+
+  var staff = store.getState().staff;
+  var member = staff.list[staff.authedId];
+
+  // TODO: Clean this thing
+  if (nextStatePath == '/login' || nextStatePath == '/') {
+    if (isAuthed == true) {
+      if (member && member.role) {
+        redirectToUserRoot(member, replace);
+        callback();
+      } else {
+        getMember(staff.authedId)
+          .then((member) => {
+            redirectToUserRoot(member, replace);
+            callback();
+          });
+      }
+    } else {
+      callback();
+    }
+  } else {
+    if (isAuthed == false) {
+      replace('/login');
+    } else {
+      if (member && member.role) {
+        if (isValidRoute(member, nextStatePath)) {
+          callback();
+        } else {
+          redirectToUserRoot(member, replace);
+          callback();
+        }
+      } else {
+        getMember(staff.authedId)
+          .then((member) => {
+            if (isValidRoute(member, nextStatePath)) {
+              callback();
+            } else {
+              redirectToUserRoot(member, replace);
+              callback();
+            }
+          });
+      }
+    }
+  }
+}
 
 var rootEl = document.getElementById('app');
 
 render(
   <AppContainer>
-    <App />
+    <App store={store} history={history} />
   </AppContainer>, rootEl
 );
 
@@ -17,7 +91,7 @@ if (module.hot) {
     const NextApp = require('./app').default;
     render(
       <AppContainer>
-         <NextApp />
+         <NextApp store={store} history={history} />
       </AppContainer>,
       rootEl
     );
