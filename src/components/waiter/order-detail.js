14338f1985c8f9bab0bf84a1e06045e85350e8f7
Richard Roncancio <batusai513@msn.com>
2016-06-05 21:16:55 -0500

    more changes

--- src/components/waiter/order-detail.js
+++ src/components/waiter/order-detail.js
@@ -1,31 +1,17 @@
 import React, { PropTypes } from 'react';
+import { OrderItems } from 'components/order-items';
 import {objectToArray} from 'helpers/format-helpers';
 
 export default function OrderDetail ({order, buttons}) {
   return (
-    <article className="panel panel--default panel--large" style={{clear: 'both'}}>
+    <article className="panel panel--large" style={{clear: 'both'}}>
       <div className="panel__head">
         <h1 className="panel__title">
           Orden # {order.orderNumber}
         </h1>
       </div>
-      <div className="panel__body">
-        <ul>
-          {
-            objectToArray(order.items || {}).map( (item, idx) => {
-            return (
-              <li key={idx}>
-                {item.name} - cantidad: {item.quantity}
-                {
-                  item.comment ?
-                  <p>{item.comment}</p> :
-                  null
-                }
-              </li>
-              );
-            })
-          }
-        </ul>
+      <div className="">
+        <OrderItems items={objectToArray(order.items || {})} />
       </div>
       <div className="panel__footer">
         {buttons}
